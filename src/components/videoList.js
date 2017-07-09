8535895d25b5139561e330bb7540f765ecc556e4
manasram <b.manasram@gmail.com>
2017-07-09 14:19:52 -0500

    second commit

---
+++ src/components/videoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import VideoListItem from './videoListItem'
+
+const VideoList = (props) => {
+    const VideoItems = props.videos.map((video) => {
+        return <VideoListItem video={video} />
+    });
+    return(
+        <ul className="col-md-4 list group">
+            {VideoItems}
+        </ul>
+
+
+    );
+};
+
+export default VideoList
--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchBar';
-
+import VideoList from './components/videoList'
 const API_KEY = 'AIzaSyCuDFiMe_8T81rZ_E8B-_CL_6uV7MPi3f4';
 
-const App = () => {
-    return (
+
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state= { videos: [] };
+        YTSearch({key : API_KEY, term:'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render(){
+
+     return (
         <div>
         <SearchBar />
+            <VideoList videos= {this.state.videos} />
     </div>
     );
-};
+}
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
